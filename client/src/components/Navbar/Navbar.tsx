1c7ce8a5b673b42829bab4f5205be4f79a9614ae
manishapariyar <manishapariyar196@gmail.com>
2025-12-16 00:12:55 +0530

    `Refactor Navbar component: update styles, icons, and layout`

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,59 +2,77 @@ import { useState } from "react";
 import logo from "../../assets/logo.png";
 import { MdOutlineMenu } from "react-icons/md";
 import { CiSearch } from "react-icons/ci";
-import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 import { BiSolidDownArrow } from "react-icons/bi";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="w-full h-20 bg-white shadow-md flex items-center px-6">
-      <div className="w-[80%] mx-auto flex items-center">
-        <div className="flex gap-3">
+    <nav className="w-full bg-white shadow-md px-4 md:px-6">
+
+
+
+      <div className="max-w-6xl mx-auto h-22 flex items-center justify-between">
+
+
+        <div className="flex items-center gap-3">
+
           <button
-            className="md:hidden text-2xl mt-5"
+            className="md:hidden text-2xl"
             onClick={() => setOpen(!open)}
           >
+
             <MdOutlineMenu />
           </button>
           {/* Logo */}
-          <div className="flex items-center mr-4">
-            <img src={logo} alt="Logo" className="w-34 md:w-40 ml-8 " />
+          <div className="flex items-center">
+            <img
+              src={logo}
+              alt="Logo"
+              className="w-28 sm:w-32 md:w-40 object-contain"
+            />
+
           </div></div>
 
         {/* Desktop Menu */}
-        <div className="hidden md:flex gap-8 text-sm ml-20">
-          <p className="cursor-pointer hover:text-blue-600 text-zinc-600 flex gap-0.5">Jobs
-            <BiSolidDownArrow className="mt-1 text-sm" />
+        <div className="hidden md:flex gap-8 text-sm">
+
+          <p className="cursor-pointer hover:text-[#013376] text-zinc-600 flex gap-0.5">Jobs
+            <BiSolidDownArrow className="text-xs" />
+
           </p>
 
-          <p className="cursor-pointer hover:text-blue-600 text-zinc-600 flex gap-0.5 ">Internships
-            <BiSolidDownArrow className="mt-1 text-sm" />
+          <p className="cursor-pointer hover:text-[#013376] text-zinc-600 flex gap-0.5 ">Internships
+            <BiSolidDownArrow className="text-xs" />
+
           </p>
         </div>
+        <div className="flex items-center gap-3 md:gap-6">
 
-        {/* Search Bar (hidden on mobile) */}
-        <div className="flex items-center gap-4">
-          <div className=" ml-40 flex items-center relative justify-bewteen  ">
-            <CiSearch className="absolute  mt-2 text-gray-400 text-xl m-2" />
+          <div className=" flex items-center relative justify-between w-full ">
+            <CiSearch className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-400 text-lg" />
+
             <input
               type="text"
               placeholder="Search"
-              className="w-full border border-gray-300 rounded-xl pl-7 py-1 outline-none  text-l mt-1 "
+              className="border border-gray-300 rounded-xl pl-8 py-1 w-32 sm:w-44 md:w-60 outline-none"
+
             />
+
           </div>
 
           {/* Buttons (Desktop) */}
           <div className="hidden md:flex  gap-4 border-r border-l px-4 border-gray-300">
-            <button className="text-blue-500 px-4 py-1 rounded-lg border border-blue-500 hover:bg-blue-50">
+            <button className="text-[#013376] px-4 py-1 rounded-lg border border-[#013376] hover:bg-blue-50">
               Login
             </button>
-            <button className="bg-blue-600 text-white px-4 py-1 rounded-lg hover:bg-blue-700">
+            <button className="bg-[#013376] text-white px-4 py-1 rounded-lg hover:bg-blue-700">
               Register
             </button>
           </div>
-          <button className=" hidden md:block text-blue-600  cursor-pointer text-sm ">
+          <button className="hidden md:flex items-center text-[#013376] text-sm whitespace-nowrap cursor-pointer hover:bg-blue-700">
+
             Register as Employer
             <IoIosArrowForward className="inline-block  " />
           </button>
@@ -69,12 +87,17 @@ const Navbar = () => {
 
         {/* Mobile Menu */}
         {open && (
-          <div className="absolute top-0 left-0 w-[40%] bg-white shadow-md flex flex-col  items-start gap-4 p-4 md:hidden h-full">
-            <p className="cursor-pointer hover:text-blue-600 text-zinc-700 ">Internships</p>
-            <p className="cursor-pointer hover:text-blue-600 text-zinc-700 ">Jobs</p>
-            <p className="cursor-pointer hover:text-blue-600 text-zinc-700 ">Contact Us</p>
+          <div className="fixed top-0 left-0 w-3/4 max-w-xs h-full bg-white shadow-lg p-4 md:hidden z-50">
 
-            <div className=" w-full border-b mb-2 border-gray-300 ">
+
+            <div className="flex flex-col gap-4">
+              <p className="cursor-pointer hover:text-blue-600 text-zinc-700 ">Internships</p>
+              <p className="cursor-pointer hover:text-blue-600 text-zinc-700 ">Jobs</p>
+              <p className="cursor-pointer hover:text-blue-600 text-zinc-700 ">Contact Us</p>
+
+            </div>
+
+            <div className=" w-full border-b  mt-6 mb-6 border-gray-300 ">
 
             </div>
 
